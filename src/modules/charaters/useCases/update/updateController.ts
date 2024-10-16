44066d64508879634dd4055121a744491c52c02d
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2024-10-16 11:26:41 +0900

    refactor: update add error treatment

--- src/modules/charaters/useCases/update/updateController.ts
+++ src/modules/charaters/useCases/update/updateController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import updateCharacterService from "./updateService";
+import { NotFoundError } from "@/helpers/errors/apiErrors";
 
 export default async function updateCharacterController(req: Request, res: Response):Promise<any> {
     const id = parseInt(req.params.id)
@@ -8,6 +9,9 @@ export default async function updateCharacterController(req: Request, res: Respo
         const update = await updateCharacterService(id, name, speciesId, affiliationId, homePlanetId)
         return res.status(200).json(update)
     } catch (error) {
+        if (error instanceof NotFoundError) {
+            return res.status(error.statusCode).send(error.message);
+        }
         return res.sendStatus(500);
     }
 
--- src/modules/charaters/useCases/update/updateService.ts
+++ src/modules/charaters/useCases/update/updateService.ts
@@ -1,6 +1,9 @@
+import { NotFoundError } from "@/helpers/errors/apiErrors";
 import characterRepository from "../../repositories/charactersRepositories";
 
 export default async function updateCharacterService(id: number, name: string, speciesId: number, affiliationId:number , homePlanetId:number) {
+    const character = await characterRepository.findById(id)
+    if(character===null) throw new NotFoundError("Not Found")
     const update = await characterRepository.updateCharacter(id, name, speciesId, affiliationId , homePlanetId)
     return update
     
