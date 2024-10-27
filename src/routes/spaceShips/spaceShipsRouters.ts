7db35756ad421608d39953ef8d1d458137a94592
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2024-10-27 19:11:51 +0900

    refactor: spaceship route

--- src/routes/spaceShips/spaceShipsRouters.ts
+++ src/routes/spaceShips/spaceShipsRouters.ts
@@ -4,6 +4,7 @@ import findAllCharactersController from "@/modules/charaters/useCases/findAll/fi
 import findByIdCharacterController from "@/modules/charaters/useCases/findById/findByIdController";
 import { spaceShipSchema, updateSpaceShipSchema } from "@/modules/spaceShips/schemas/spaceShipSchema";
 import CreateSpaceShipController from "@/modules/spaceShips/useCases/create/createController";
+import deleteSpaceShipController from "@/modules/spaceShips/useCases/delete/deleteController";
 import updateSpaceShipController from "@/modules/spaceShips/useCases/update/updateController";
 import { Router } from "express";
 
@@ -13,6 +14,6 @@ spaceShipRouter.post('/', authValidation, validateSchema(spaceShipSchema), Creat
 spaceShipRouter.get('/', findAllCharactersController)
 spaceShipRouter.get('/:id',findByIdCharacterController)
 spaceShipRouter.put('/:id',authValidation, validateSchema(updateSpaceShipSchema), updateSpaceShipController)
-spaceShipRouter.delete('/:id',authValidation,)
+spaceShipRouter.delete('/:id',authValidation, deleteSpaceShipController) 
 
 export default spaceShipRouter
