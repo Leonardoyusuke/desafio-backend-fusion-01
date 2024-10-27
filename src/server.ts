87c94aa8492b87c84a5161a1e02d540f8fd26b77
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2024-10-27 22:34:18 +0900

    refactor: port config

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import 'module-alias/register';
 import app from './app';
 
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running in port: ${port}`));
