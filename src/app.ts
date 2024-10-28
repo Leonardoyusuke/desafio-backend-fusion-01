0c3d62709e9fd1f37b011c45006217d65caacf7a
Leonardo Yusuke Hirano <leoyuhirano@gmail.com>
2024-10-28 12:35:32 +0900

    refactor: for deploy

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,11 @@ app.use(router);
 
 app.use(errorHandler)
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
+
 connectDb();
+
+
 
 export default app
